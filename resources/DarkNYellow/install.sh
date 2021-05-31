5c1ebb494cf09bff7092527ecf81218184d4d148
Om Pawar <76126226+Voidenz@users.noreply.github.com>
2021-05-31 10:36:20 +0530

    Update install.sh

--- resources/DarkNYellow/install.sh
+++ resources/DarkNYellow/install.sh
@@ -1,7 +1,7 @@
 php /var/www/pterodactyl/artisan down
 cd /var/www/pterodactyl/resources/scripts
 rm -rf main.css
-wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/DarkNYellow/main.css
+wget https://raw.githubusercontent.com/Voidenz/PteroFreeStuffinstaller/main/resources/DarkNYellow/main.css
 rm -rf index.tsx
 wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/DarkNRed/index.tsx
 cd /var/www/pterodactyl/resources/views/layouts/
@@ -17,4 +17,4 @@ yarn add @emotion/react
 yarn build:production
 clear
 php /var/www/pterodactyl/artisan up
-echo "Dark N Yellow theme Installed"
+echo "Dark N Blue theme Installed"
